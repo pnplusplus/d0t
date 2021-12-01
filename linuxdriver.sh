c52bb131fce878b03d524dda3d475c0ca201bed6
pnplusplus <52704636+pnplusplus@users.noreply.github.com>
2021-12-01 07:02:56 +0700

    Update linuxdriver.sh

--- linuxdriver.sh
+++ linuxdriver.sh
@@ -3,4 +3,4 @@ if [ `whoami` != root ]; then
     echo "Please run this script as root or using sudo."
     exit
 fi
-sudo pacman --noconfirm -S xorg-server xf86-video-intel mesa vulkan-intel vulkan-mesa-layers
+sudo pacman --noconfirm -S xorg-server xf86-video-intel xf86-input-synaptics mesa vulkan-intel vulkan-mesa-layers
